3305d04be557b15a2cbe4ac291830499c312ecc3
2DAW Aitor Trujillo Pablo <atrujillop05@informatica.iesvalledeljerteplasencia.es>
2025-10-24 17:48:49 +0200

    Se ha realizado el ejercicio 2, se ha creado una pagina que muestre un card por cada tarea. Se ha incluido el servidor de back como express-server

---
+++ Ejercicio2/js/Ejercicio2.js
@@ -0,0 +1,37 @@
+var peticionPrincipal = new XMLHttpRequest();
+peticionPrincipal.open("GET","http://localhost:3000/tasks")
+peticionPrincipal.addEventListener("readystatechange", procesarPeticion)
+peticionPrincipal.send()
+
+function crearCardTarea(tarea, container){
+    var div = document.createElement("div");
+
+    div.className = "col-md-4"
+
+    div.innerHTML = `
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <h5 class="card-title">${tarea.titulo}</h5>
+                    <p class="card-text">${tarea.descripcion}</p>
+                    <span class="badge badge-${(tarea.estado === "completada") ? "completada" : (tarea.estado === "haciendo") ? "haciendo" : "pendiente" }">${tarea.estado}</span>
+                    <p class="text-muted small mt-2">${tarea.fecha_creacion}</p>
+                </div>
+            </div>
+    `;
+
+    container.appendChild(div)
+}
+
+function procesarResultado(resultado){
+    var divContenedorTareas = document.getElementById("divContenedorTareas");
+    for (var tarea of resultado) {
+        crearCardTarea(tarea, divContenedorTareas);
+    }
+}
+
+function procesarPeticion(event){
+    if (this.readyState == 4 && this.status == 200) {
+        let resultado = JSON.parse(this.responseText);
+        procesarResultado(resultado);
+    }
+}
