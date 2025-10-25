3301bbe23326fc29483835b51b477ca96f890597
AitorTrujillo9 <atrujillop05@informatica.iesvalledeljerteplasencia.es>
2025-10-25 16:58:16 +0200

    modal creado(Se ve pero no funciona)

---
+++ Ejercicio2/js/ModalCrearTarea.js
@@ -0,0 +1,25 @@
+export class ModalNuevaTarea {
+  constructor() {
+    this.modal = null;
+  }
+
+  async cargar() {
+    const response = await fetch("modales/modalNuevaTarea.html");
+    const html = await response.text();
+    document.body.insertAdjacentHTML("beforeend", html);
+
+    this.modal = new bootstrap.Modal(document.getElementById("modalNuevaTarea"));
+    const form = document.getElementById("formNuevaTarea");
+
+    // Al enviar, cerramos modal y "volvemos al inicio"
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.modal.hide();
+      window.scrollTo(0, 0); // Vuelve arriba
+    });
+  }
+
+  mostrar() {
+    this.modal.show();
+  }
+}
